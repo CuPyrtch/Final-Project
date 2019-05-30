77e46c5bfa365ecbfde3410f7b1c606cdbbf38b1
CuPyrtch <46973325+CuPyrtch@users.noreply.github.com>
2019-05-30 14:04:55 -0400

    Updated from Brython Server: 5/30/2019 2:04:54 PM

--- project.py
+++ project.py
@@ -6,9 +6,9 @@ class SpaceShip(Sprite):
     """
     Animated space ship
     """
-    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-        Frame(227,0,65,125), 4, 'vertical')
-
+    asset = ImageAsset("images/player1.png", 
+        Frame(119,90,50,50), 3, 'vertical')
+        
     def __init__(self, position):
         super().__init__(SpaceShip.asset, position)
         self.vx = 0
@@ -17,43 +17,24 @@ class SpaceShip(Sprite):
         self.rotation = 0
         self.thrust = 0
         self.thrustframe = 1
-        SpaceGame.listenKeyEvent("keydown", "w", self.thrustOn)
-        SpaceGame.listenKeyEvent("keyup", "w", self.thrustOff)
         SpaceGame.listenKeyEvent("keydown", "a", self.RotateLOn)
         SpaceGame.listenKeyEvent("keyup", "a", self.RotateLOff)
         SpaceGame.listenKeyEvent("keydown", "d", self.RotateROn)
         SpaceGame.listenKeyEvent("keyup", "d", self.RotateROff)
-        self.fxcenter = self.fycenter = 0.5
         
 
     def step(self):
         self.x += self.vx
         self.y += self.vy
         self.rotation += self.vr
-        # manage thrust animation
-        if self.thrust == 1:
-            self.setImage(self.thrustframe)
-            self.thrustframe += 1
-            if self.thrustframe == 4:
-                self.thrustframe = 1
-        else:
-            self.setImage(0)
     def RotateLOn(self, event):
-        self.vr = .01
+        self.vx = -2
     def RotateLOff(self, event):
-        self.vr = 0
-    def RotateROn(self, event):
-        self.vr = -.01
-    def RotateROff(self, event):
-        self.vr = 0
-    def thrustOn(self, event):
-        self.thrust = 1
-        self.vx = -math.sin(self.rotation)
-        self.vy = -math.cos(self.rotation)
-    def thrustOff(self, event):
-        self.thrust = 0
         self.vx = 0
-        self.vy = 0
+    def RotateROn(self, event):
+        self.vx = 2
+    def RotateROff(self, event):
+        self.vx = 0
 
 
 class SpaceGame(App):
@@ -67,9 +48,7 @@ class SpaceGame(App):
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(bg_asset, (0,0))
-        SpaceShip((100,100))
-        SpaceShip((150,150))
-        SpaceShip((200,50))
+        SpaceShip((500,470))
 
     def step(self):
         for ship in self.getSpritesbyClass(SpaceShip):
