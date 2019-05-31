39e63e04bdf121f17322b626bc15549240c83d4e
CuPyrtch <46973325+CuPyrtch@users.noreply.github.com>
2019-05-31 08:48:37 -0400

    Updated from Brython Server: 5/31/2019 8:48:36 AM

--- project.py
+++ project.py
@@ -1,26 +1,48 @@
-from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, CircleAsset
 from ggame import SoundAsset, Sound, TextAsset, Color
 import math
 from time import time
-class SpaceShip(Sprite):
-    """
-    Animated space ship
-    """
+
+class Blast(Sprite):
+    asset = ImageAsset("images/player1.png", Frame(119,90,50,50), 1, 'vertical')
+    collisionasset = CircleAsset(4)
+    
+    def __init__(self, app):
+        super().__init__(Bullet.asset, Bullet.collisionasset, (0,0), (0,0))
+        self.visible = False
+        self.firing = False
+        self.time = 0
+        
+    def shoot(self, position, velocity, time):
+        self.position = position
+        self.vx = velocity[0]
+        self.vy = velocity[1]
+        self.time = time
+        self.visible = True
+        self.firing = True
+        
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+
+class Shooter(Sprite):
     asset = ImageAsset("images/player1.png", 
-        Frame(119,90,50,50), 3, 'vertical')
+        Frame(119,90,50,50), 1, 'vertical')
         
     def __init__(self, position):
-        super().__init__(SpaceShip.asset, position)
+        super().__init__(Shooter.asset, position)
         self.vx = 0
         self.vy = 0
         self.vr = 0
         self.rotation = 0
         self.thrust = 0
-        self.thrustframe = 1
-        SpaceGame.listenKeyEvent("keydown", "a", self.RotateLOn)
-        SpaceGame.listenKeyEvent("keyup", "a", self.RotateLOff)
-        SpaceGame.listenKeyEvent("keydown", "d", self.RotateROn)
-        SpaceGame.listenKeyEvent("keyup", "d", self.RotateROff)
+        SpaceShooter.listenKeyEvent("keydown", "a", self.RotateLOn)
+        SpaceShooter.listenKeyEvent("keyup", "a", self.RotateLOff)
+        SpaceShooter.listenKeyEvent("keydown", "d", self.RotateROn)
+        SpaceShooter.listenKeyEvent("keyup", "d", self.RotateROff)
+        SpaceShooter.listenKeyEvent("keydown", "space", self.ShootOn)
+        SpaceShooter.listenKeyEvent("keyup", "space", self.ShootOff)
         
 
     def step(self):
@@ -35,25 +57,47 @@ class SpaceShip(Sprite):
         self.vx = 2
     def RotateROff(self, event):
         self.vx = 0
+    def ShootOn(self, event):
+        shoot = True
+    def ShootOff(self, event):
+        shoot = False
 
+class Enemy(Sprite):
+    asset = ImageAsset("images/191-1916209_space-invaders-sprites.png", 
+        Frame(140, 200,50,50), 1, 'vertical')
+    collisionasset = CircleAsset(40)
+    
+    def __init__(self, position):
+        super().__init__(Enemy.asset, position)
+        self.vx = 0
+        self.vy = 1
+        self.vr = 0
+        self.rotation = 0
+        self.thrust = 0
+        
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
 
-class SpaceGame(App):
-    """
-    Tutorial4 space game example.
-    """
+class SpaceShooter(App):
     def __init__(self):
         super().__init__()
-        # Background
         black = Color(0, 1)
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(bg_asset, (0,0))
-        SpaceShip((500,470))
+        Shooter((500,470))
+        Enemy((100, 50))
+   
+    def step(self):
+        for ship in self.getSpritesbyClass(Enemy):
+            ship.step()
 
     def step(self):
-        for ship in self.getSpritesbyClass(SpaceShip):
+        for ship in self.getSpritesbyClass(Shooter):
             ship.step()
 
         
-myapp = SpaceGame()
+myapp = SpaceShooter()
 myapp.run()
